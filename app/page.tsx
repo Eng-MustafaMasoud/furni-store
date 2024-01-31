51de82ba39eccde04284a3d781ba1d9af84884be
Eng-MustafaMasoud <mustafamasoud95@gmail.com>
2024-01-31 05:35:12 +0200

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 
 const DynamicHome=dynamic(()=>  import("@/components/Home") ,
 {ssr:false,
-loading:()=><Loader/>}
+}
 
 )
 export default function Home() {
--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import couch from '@/public/images/spinner-5.gif'
+import spinner from '@/public/images/spinner-5.gif'
 import Image from 'next/image'
 const Loader = () => {
   return (
     <div  className="flex flex-col h-screen items-center  justify-center  w-full scrollbar-hide overflow-hidden ">
-        <Image src={couch} alt="couch" className=' object-contain'/>
+        <Image src={spinner} alt="couch" className=' object-contain'/>
         
     </div>
   )
